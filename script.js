b277c0a200f375a58a6cd6cbaf5d651cb526976c
Pedro Soares <pedroneto.ps180@gmail.com>
2020-06-09 08:59:10 -0300

    seguencia de produtos

---
+++ script.js
@@ -0,0 +1,14 @@
+var num=0;
+window.setInterval(function(){
+  num++;
+  var classes=['','out','ready'];
+  $('#watch').removeClass();
+  $('#watch').addClass(classes[num%3])
+  $('#phone').removeClass();
+  $('#phone').addClass(classes[(num+2)%3]);
+  $('#laptop').removeClass();
+  $('#laptop').addClass(classes[(num+1)%3]);
+  $('.quote span').removeClass();
+  $('.quote span:nth-child('+ (num%3+1) +')').addClass('highlight');
+  console.log(num%3+1);
+},900)
